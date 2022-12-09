7d5684c1a9b54ec83ca041ebdf4e8de2f3d352fb
hothreatlonglong <wjl13974749712@163.com>
2022-12-09 16:58:56 +0800

    多态的应用

--- java基础/chapter08/src/com/wjl/ploy_/ployparameter_/PloyParameter.java
+++ java基础/chapter08/src/com/wjl/ploy_/ployparameter_/PloyParameter.java
@@ -8,19 +8,22 @@ public class PloyParameter {
     //测试方法
     public static void testWork(Employee employee){
         if(employee instanceof Manage){
-            ((Manage)employee).manage();
+            ((Manage)employee).manage();//向下转型
         } else if (employee instanceof Work) {
-            ((Work)employee).work();
+            ((Work)employee).work(); //向下转型
         }else {
             System.out.println("类型错误！");
         }
     }
 
     public static void main(String[] args) {
+        //创建两个对象
         Employee employee = new Manage("张三",12,23456,4432);
         Employee employee1 = new Work("李四",12,3232);
+        //计算年工资
         System.out.println(showEmpAnnual(employee));
         System.out.println(showEmpAnnual(employee1));
+        //测试方法
         testWork(employee);
         testWork(employee1);
     }
