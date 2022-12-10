79348c4b46fe96f1bb7dd4d3a87ace20e3af7b22
hothreatlonglong <wjl13974749712@163.com>
2022-12-10 10:58:42 +0800

    对于的javaObject类的详解： 2、finalize方法，实现对对象的资源回收

---
+++ java基础/chapter08/src/com/wjl/object_/Finalize_.java
@@ -0,0 +1,33 @@
+package com.wjl.object_;
+
+public class Finalize_ {
+    public static void main(String[] args) {
+        //创建一个对象
+        Car car = new Car("宝马");
+        car = null;
+        //调用gc机制
+        System.gc();
+        System.out.println("程序退出运行");
+    }
+}
+class Car{
+    private String name;
+
+    public Car(String name) {
+        this.name = name;
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        System.out.println("汽车："+this.name+"被销毁了");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
