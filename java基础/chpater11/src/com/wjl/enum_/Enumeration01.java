c2a077f84e72cb24d04adc04d54a5c1579c66749
hothreatlonglong <wjl13974749712@163.com>
2022-12-20 14:37:35 +0800

    自定义枚举的使用

---
+++ java基础/chpater11/src/com/wjl/enum_/Enumeration01.java
@@ -0,0 +1,52 @@
+package com.wjl.enum_;
+
+/**
+ * @author WangJunLong
+ * @version JDK 18
+ * @QQ 2396595751
+ * @VX Karl_Is_Belief
+ * @E-Mail wjl13974749712@163.com
+ * 自定义枚举的使用
+ */
+public class Enumeration01 {
+    public static void main(String[] args) {
+        System.out.println(Season.SPRING);
+        System.out.println(Season.SUMMER);
+        System.out.println(Season.AUTUMN);
+        System.out.println(Season.WINTER);
+    }
+}
+
+//类
+class Season{
+    private String name;
+    private String desc;
+
+    public static final Season SPRING = new Season("春天","温暖");
+    public static final Season SUMMER = new Season("夏天","炎热");
+    public static final Season AUTUMN = new Season("秋天","凉爽");
+    public static final Season WINTER = new Season("冬天","寒冷");
+    //1、把构造器私有化
+    //2、把Setxx方法去掉
+    //3、创建限定值为静态变量
+    public Season(String name, String desc) {
+        this.name = name;
+        this.desc = desc;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    @Override
+    public String toString() {
+        return "Season{" +
+                "name='" + name + '\'' +
+                ", desc='" + desc + '\'' +
+                '}';
+    }
+}
